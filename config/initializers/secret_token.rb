bac3ffded1a162ea000895be3bcccb2cd57518c0
Matthew Mongeau <matt@toastyapps.com>
2012-05-19 15:34:28 -0400

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memori::Application.config.secret_token = 'c467bc4b5c085c5836c0c70ff1bdbcfa9aa02366c2b7e93c8df2799228f4082a39e2cf11e4a26db995a5c9d2bdd653f25ca8a450df97c2812fe1266343973208'
