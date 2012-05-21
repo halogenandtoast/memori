7142c519fa968ffbf2809ff59d5297b65f818798
Matthew Mongeau <matt@toastyapps.com>
2012-05-20 21:32:07 -0400

    Fix color on card show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,5 +4,11 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
     @record = current_user.record_for_card(@card)
+
+    @color = case @record.bucket
+             when 0 then "brown"
+             when 1 then "yellow"
+             else "green"
+             end
   end
 end
