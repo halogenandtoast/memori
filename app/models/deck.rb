62e9ae7970128062b0d40b3b25f437c49d03d763
Matthew Mongeau <matt@toastyapps.com>
2012-05-21 22:23:59 -0400

    Implement deck sizes

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,17 @@
 class Deck < ActiveRecord::Base
   attr_accessible :name
   has_many :cards
+
+  def size
+    length = cards.count
+    if length < 151
+      "small"
+    elsif length < 401
+      "medium"
+    elsif length < 1001
+      "large"
+    else
+      "x-large"
+    end
+  end
 end
